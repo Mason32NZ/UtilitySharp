40a964220f7d9cfbc9abaf2fb616a955fdf4a303
Mason Galland <mason.gnz@gmail.com>
2018-07-26 19:58:39 +1200

    Added DateTimeFormatStringToRegex map.

--- src/UtilitySharp/StringHelper.cs
+++ src/UtilitySharp/StringHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -170,6 +171,69 @@ namespace UtilitySharp
                 a = str.Substring(0, b) + str.Substring(b).Replace(oldVal, newVal);
             }
             return a;
+        }
+
+        public static string DateTimeFormatStringToRegex(string str)
+        {
+            var map = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>( "d", "([1-3][0-9]|[1-9])"),
+                new KeyValuePair<string, string>( "dd", "[0-3][0-9]" ),
+                new KeyValuePair<string, string>( "ddd", "[A-Za-z]{3)" ), // TODO: Use | case.
+                new KeyValuePair<string, string>( "dddd", "[A-Za-z]{6,9)" ), // TODO: Use | case.
+                new KeyValuePair<string, string>( "f", "[0-9]" ),
+                new KeyValuePair<string, string>( "ff", "[0-9]{2)" ),
+                new KeyValuePair<string, string>( "fff", "[0-9]{3)" ),
+                new KeyValuePair<string, string>( "ffff", "[0-9]{4)" ),
+                new KeyValuePair<string, string>( "fffff", "[0-9]{5)" ),
+                new KeyValuePair<string, string>( "ffffff", "[0-9]{6)" ),
+                new KeyValuePair<string, string>( "fffffff", "[0-9]{7)" ),
+                new KeyValuePair<string, string>( "F", "[1-9]" ),
+                new KeyValuePair<string, string>( "FF", "[1-9]{2)" ),
+                new KeyValuePair<string, string>( "FFF", "[1-9][0-9][1-9]" ),
+                new KeyValuePair<string, string>( "FFFF", "[1-9][0-9]{2)[1-9]" ),
+                new KeyValuePair<string, string>( "FFFFF", "[1-9][0-9]{3)[1-9]" ),
+                new KeyValuePair<string, string>( "FFFFFF", "[1-9][0-9]{4)[1-9]" ),
+                new KeyValuePair<string, string>( "FFFFFFF", "[1-9][0-9]{5)[1-9]" ),
+                new KeyValuePair<string, string>( "g", "[A-B].?[C-D].?" ), // TODO: Use | case.
+                new KeyValuePair<string, string>( "gg", "[A-B].?[C-D].?" ), // TODO: Use | case.
+                new KeyValuePair<string, string>( "h", "(1[0-2]|[1-9])" ),
+                new KeyValuePair<string, string>( "hh", "(1[0-2]|0[1-9])" ),
+                new KeyValuePair<string, string>( "H", "(2[0-3]|1[0-9]|[0-9])" ),
+                new KeyValuePair<string, string>( "HH", "(2[0-3]|1[0-9]|0[0-9])" ),
+                new KeyValuePair<string, string>( "K", "[+-][0-9]{2):[0-9]{2)" ),
+                new KeyValuePair<string, string>( "m", "([1-5][0-9]|[0-9])" ),
+                new KeyValuePair<string, string>( "mm", "[0-5][0-9]" ),
+                new KeyValuePair<string, string>( "M", "(1[0-2]|[1-9])" ),
+                new KeyValuePair<string, string>( "MM", "(1[0-2]|0[1-9])" ),
+                new KeyValuePair<string, string>( "MMM", "([A-Za-z]{3))?" ), // TODO: Use | case.
+                new KeyValuePair<string, string>( "MMMM", "([A-Za-z]{3,9))?" ), // TODO: Use | case.
+                new KeyValuePair<string, string>( "s", "([1-5][0-9]|[0-9])" ),
+                new KeyValuePair<string, string>( "ss", "[0-5][0-9]" ),
+                new KeyValuePair<string, string>( "t", "[PA]" ),
+                new KeyValuePair<string, string>( "tt", "(PM|AM)" ),
+                new KeyValuePair<string, string>( "y", "([1-9][0-9]|[0-9])" ),
+                new KeyValuePair<string, string>( "yy", "[0-9]{2)" ),
+                new KeyValuePair<string, string>( "yyy", "[0-9]{3,4)" ),
+                new KeyValuePair<string, string>( "yyyy", "[0-9]{4)" ),
+                new KeyValuePair<string, string>( "z", "[+-][0-9]" ),
+                new KeyValuePair<string, string>( "zz", "[+-][0-9]{2)" ),
+                new KeyValuePair<string, string>( "zzz", "[+-][0-9]{2):[0-9]{2)" ),
+                new KeyValuePair<string, string>( ":", "[:.]" ),
+                new KeyValuePair<string, string>( "/", "[/-.]" )
+            };
+            map.Reverse();
+
+            var list = new List<string>();
+
+            foreach (var format in map)
+            {
+                str = "(" + str.Replace(format.Key, format.Value) + ")";
+            }
+
+            //string t = CultureInfo.CurrentCulture.DateTimeFormat. // Check out this later.
+
+            return str;
         }
     }
 }
