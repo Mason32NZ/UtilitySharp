8ce58ab5d6afe1d930451866731ab3379a81a33d
Mason Galland <mason.gnz@gmail.com>
2018-09-06 01:09:44 +1200

    Started to add the ValidateData() method.

---
+++ src/UtilitySharp/Models/DataValidationError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilitySharp.Models
+{
+    public class DataValidationError
+    {
+        public int? Row { get; set; }
+        public int? Column { get; set; }
+        public string Message { get; set; }
+    }
+}
